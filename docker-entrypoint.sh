2bb7624745ab101fe4d65768c0ea843fa5977db3
Eric <wdc-zhy@163.com>
2018-09-27 13:59:42 +0800

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 chmod 644 /data/etc/my.cnf
 echo "执行mysql_install_db --defaults-file=/data/etc/my.cnf --skip-name-resolve --datadir=/data/database/ --force"
-mysql_install_db  --defaults-file=/data/etc/my.cnf --skip-name-resolve --datadir=/data/database/ --force --basedir=/usr/bin
+mysql_install_db  --defaults-file=/data/etc/my.cnf --skip-name-resolve --datadir=/data/database/ --force --basedir=/usr
 set -eo pipefail
 # set -x
 
